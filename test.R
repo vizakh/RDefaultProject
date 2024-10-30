eb1c8cd5458953f9b434867778a4757bf7825eb0
Volodymyr Zakharenko <vladimir.s.zakharenko@gmail.com>
2024-10-30 18:44:39 +0200

    Tried linear regression with trend and seasonality

--- test.R
+++ test.R
@@ -41,16 +41,27 @@ plot(data$time_index, data$number_sold,
      xlim =c(0, max(data$time_index) + forecast_interval))
 lines(future_time_index, forecast_trend, col = "blue", lty = 2)
 # ------------------------------------------------------------------------------
+data$month_factor <- as.factor(format(data$Date, "%m"))  # Месяц как фактор для сезонности
+print(data)
 
 # Линейная регрессия с трендом и сезонностью
-linear_seasonal_model <- lm(defaults ~ time_index + month_factor, data = data)
+linear_seasonal_model <- lm(number_sold ~ time_index + month_factor, data = data)
 summary(linear_seasonal_model)
 
 # Прогноз на будущее
-future_month_factor <- as.factor(rep(1:12, length.out = 12))  # 12 месяцев вперёд
-future_data <- data.frame(time_index = future_time_index, month_factor = future_month_factor)
+future_time_index <- (nrow(data) + 1):(nrow(data) + forecast_interval)  # Прогнозируем на 12 месяцев вперёд
+future_month_factor <- as.factor(c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"))
+future_data <- data.frame(time_index = future_time_index)
+future_data$month_factor <- future_month_factor
+print(future_data)
+str(future_data)
+str(data)
+
+# Построение прогноза
 forecast_seasonal <- predict(linear_seasonal_model, newdata = future_data)
 
 # График прогноза
-plot(data$time_index, data$defaults, type = "l", main = "Линейная регрессия с трендом и сезонностью", xlab = "Время", ylab = "Дефолты")
+plot(data$time_index, data$number_sold, 
+     type = "l", main = "Линейная регрессия с трендом и сезонностью", xlab = "Время", ylab = "Дефолты",
+     xlim =c(0, max(data$time_index) + forecast_interval))
 lines(future_time_index, forecast_seasonal, col = "red", lty = 2)
