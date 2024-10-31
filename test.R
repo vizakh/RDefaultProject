9b7920663d585dc2b63b65b01e2273d0a128c919
Volodymyr Zakharenko <vladimir.s.zakharenko@gmail.com>
2024-10-31 11:46:19 +0200

    Deleted ETS

--- test.R
+++ test.R
@@ -6,7 +6,7 @@ library(forecast)
 
 data <- read.table('F:/Pivdennyy/train.csv', header = TRUE, sep = ',',
                    colClasses = c("character", "NULL", "NULL", "numeric"))
-print(data)
+head(data)
 
 # Обробка даних та будування графіків-------------------------------------------
 data$Date <- ymd(data$Date)
@@ -18,10 +18,19 @@ data <- data %>%
 
 ggplot(data, aes(x = Date, y = number_sold)) +
   geom_line() + scale_x_date(date_labels = "%m-%Y")
-print(data)
+head(data)
 
 data = data[!(data$Date < as.POSIXct("2016-01-01")),]
 ggplot(data, aes(x = Date, y = number_sold)) +
+  geom_line() + scale_x_date(date_labels = "%m-%Y")
+
+monthly_data <- data %>%
+  mutate(month = ym(format(Date, "%Y-%m"))) %>%
+  group_by(month) %>%
+  summarize(number_sold = sum(number_sold))
+head(monthly_data)
+
+ggplot(monthly_data, aes(x = month, y = number_sold)) +
   geom_line() + scale_x_date(date_labels = "%m-%Y")
 
 # Лінійна регресія з трендом----------------------------------------------------
@@ -66,36 +75,13 @@ forecast_seasonal <- predict(linear_seasonal_model, newdata = future_data,
                              interval = "confidence", level = 0.95)
 
 # Графік даних та прогнозів з інтервалами
-plot(data$time_index, data$number_sold, type = "l", main = "LM with trend and seasonality", xlab = "Time", ylab = "Defaults", xlim = c(1, max(future_time_index)))
+plot(data$time_index, data$number_sold, type = "l", main = "LM with trend and seasonality", 
+     xlab = "Time", ylab = "Defaults", xlim = c(1, max(future_time_index)))
 grid()
-lines(future_time_index, forecast_seasonal[, "fit"], col = "red", lty = 1)         # Середнє значення прогнозу
-lines(future_time_index, forecast_seasonal[, "lwr"], col = "blue", lty = 2)        # Нижня границя інтервалу
-lines(future_time_index, forecast_seasonal[, "upr"], col = "blue", lty = 2)        # Верхня границя інтервалу
+lines(future_time_index, forecast_seasonal[, "fit"], col = "red", lty = 1)  # Середнє значення прогнозу
+lines(future_time_index, forecast_seasonal[, "lwr"], col = "blue", lty = 2) # Нижня границя інтервалу
+lines(future_time_index, forecast_seasonal[, "upr"], col = "blue", lty = 2) # Верхня границя інтервалу
 
 # Легенда до графіку
 legend("bottomright", legend = c("Middle forecast", "Confidence interval (95%)"),
        col = c("red", "blue"), lty = c(1, 2), bty = "n")
-
-# Модель ETS--------------------------------------------------------------------
-# Временной ряд данных о дефолтах
-data_ts <- ts(data$number_sold, frequency = 12) # Предполагаем месячную сезонность
-
-# Построение модели ETS
-ets_model <- ets(data_ts, model="MAA")
-forecast_ets <- forecast(ets_model, h = 100) # Прогноз на 12 периодов
-
-# График прогноза
-plot(forecast_ets)
-
-# # Модель на основі сезонних індексів--------------------------------------------
-# # Разложение временного ряда
-# decomposed <- decompose(data_ts)
-# 
-# # Построение прогноза с использованием сезонных индексов
-# seasonally_adjusted <- data_ts - decomposed$seasonal
-# linear_model <- tslm(seasonally_adjusted ~ trend) # Прогноз с учетом тренда
-# forecast_linear <- forecast(linear_model, h = 54)
-# adjusted_forecast <- forecast_linear$mean + decomposed$seasonal[1:54] # Учёт сезонности
-# 
-# # График прогноза
-# plot(adjusted_forecast)
